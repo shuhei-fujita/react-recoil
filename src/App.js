e6077c89a03f45f9f43b1d9b9643f89dee23476f
syuheifujita <syuheifujita0117@gmail.com>
2021-12-27 10:17:07 +0900

    filter リストcount 機能追加

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import {
   RecoilRoot,
   atom,
@@ -12,12 +12,16 @@ const todoListState = atom({
   key: 'todoListState',
   default: [],
 });
+const todoListFilterState = atom({
+  key: 'todoListFilterState',
+  default: 'Show All',
+});
 
-function TodoItem({item}) {
+function TodoItem({ item }) {
   const [todoList, setTodoList] = useRecoilState(todoListState);
   const index = todoList.findIndex((listItem) => listItem === item);
 
-  const editItemText = ({target: {value}}) => {
+  const editItemText = ({ target: { value } }) => {
     const newList = replaceItemAtIndex(todoList, index, {
       ...item,
       text: value,
@@ -78,7 +82,7 @@ function TodoItemCreator() {
     setInputValue('');
   };
 
-  const onChange = ({target: {value}}) => {
+  const onChange = ({ target: { value } }) => {
     setInputValue(value);
   };
 
@@ -89,20 +93,89 @@ function TodoItemCreator() {
     </div>
   );
 }
-
 // utility for creating unique Id
 let id = 0;
 function getId() {
   return id++;
 }
 
-function TodoList() {
-  const todoList = useRecoilValue(todoListState);
+const filteredTodoListState = selector({
+  key: 'filteredTodoListState',
+  get: ({ get }) => {
+    const filter = get(todoListFilterState);
+    const list = get(todoListState);
+
+    switch (filter) {
+      case 'Show Completed':
+        return list.filter(item => item.isComplete);
+      case 'Show Uncompleted':
+        return list.filter(item => !item.isComplete);
+      default:
+        return list;
+    }
+  },
+});
+
+const todoListStatsState = selector({
+  key: 'todoListStatsState',
+  get: ({get}) => {
+    const todoList = get(filteredTodoListState);
+    const totalNum = todoList.length;
+    const totalCompletedNum = todoList.filter(item => item.isComplete).length;
+    const totalUncompletedNum = totalNum - totalCompletedNum;
+    const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum;
+
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+      percentCompleted,
+    };
+  },
+});
+
+const TodoListStats = () => {
+  const {totalNum, totalCompletedNum, totalUncompletedNum, percentCompleted} =
+    useRecoilValue(todoListStatsState);
+
+  const formattedPercentCompleted = Math.round(percentCompleted * 100);
+
+  return (
+    <ul>
+      <li>Total items: {totalNum}</li>
+      <li>Items completed: {totalCompletedNum}</li>
+      <li>Items not completed: {totalUncompletedNum}</li>
+      <li>Percent completed: {formattedPercentCompleted}</li>
+    </ul>
+  );
+};
+
+function TodoListFilters() {
+  const [filter, setFilter] = useRecoilState(todoListFilterState);
+
+  const updateFilter = ({ target: { value } }) => {
+    setFilter(value);
+  };
 
   return (
     <>
-      {/* <TodoListStats /> */}
-      {/* <TodoListFilters /> */}
+      Filter:
+      <select value={filter} onChange={updateFilter}>
+        <option value="Show All">All</option>
+        <option value="Show Completed">Completed</option>
+        <option value="Show Uncompleted">Uncompleted</option>
+      </select>
+    </>
+  );
+};
+
+function TodoList() {
+  const todoList = useRecoilValue(filteredTodoListState);
+
+  return (
+    <>
+      <TodoListStats />
+      <TodoListFilters />
       <TodoItemCreator />
 
       {todoList.map((todoItem) => (
